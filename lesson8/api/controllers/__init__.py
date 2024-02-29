8b4894eb007e8f687e544e681ae12d853bb020ea
Roman M. Gerasimov <romagrizly@gmail.com>
2024-02-29 18:58:33 +0500

    lesson 8 extend from lesson 7

---
+++ lesson8/api/controllers/__init__.py
@@ -0,0 +1,2 @@
+from .language_controller import router as items_controller
+from .test_controller import router as test_controller
---
+++ lesson8/api/server.py
@@ -0,0 +1,30 @@
+from typing import Annotated
+
+from fastapi import FastAPI, Depends, status, HTTPException
+from fastapi.security import OAuth2PasswordRequestForm
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from database import get_session
+from model import Token
+from services import UserService, AuthService
+from .controllers import test_controller
+from .controllers import items_controller
+
+
+app = FastAPI(docs_url="/")
+app.include_router(items_controller)
+app.include_router(test_controller)
+
+
+@app.post("/token")
+async def login(form_data: Annotated[OAuth2PasswordRequestForm, Depends()],
+                session: AsyncSession = Depends(get_session)):
+    user = await UserService.get_user_by_login(form_data.username, session)
+    if not user:
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST,
+                            detail="Incorrect username or password")
+    if not AuthService.verify_password(form_data.password, user.password):
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST,
+                            detail="Incorrect username or password")
+    access_token = AuthService.create_access_token_by_user(user)
+    return Token(access_token=access_token, token_type="Bearer")
